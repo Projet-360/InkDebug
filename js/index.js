bf2d0a05ad6720ea582818131e30f4f54b18cb1b
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2021-05-01 03:00:19 +0200

    ScrollTrigger !

--- js/index.js
+++ js/index.js
@@ -1,582 +1,125 @@
-
-
-window.onload = function () {
-
-
-  // FIN DES SETS DES ANIMATIONS
-
-
-  // CHANGEMENT DE LA COULEUR DU BODY
-
-  const GeneralSM = new ScrollMagic.Controller();
-  var aTM = new TimelineMax();
-  
-  // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-  // ZOOM a
-
-
-  aTM.add([
-    TweenMax.fromTo(".ink-a-js", 1, {
-      scale: 1,
-      y: "0%",
-      position: "fixed",
-      force3D: false,
-    }, {
-      scale: 130,
-      y: "0%",
-      position: "fixed",
-      ease: "expoScale(1, 130)",
-      force3D: false
-    }),
-  ]);
-
-  var aTM = new ScrollMagic.Scene({
-      duration: '100%',
-      triggerHook: 0,
-    })
-    .setTween(aTM)
-    .addTo(GeneralSM)
-
-// FIN AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
-
-
-
-// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-var bTM = new TimelineMax();
-
-bTM.add([
-
-  TweenMax.fromTo(".ink-b-js", 1, {
-    scale: 0,
-    y: "-100%",
-    position: "fixed",
-    opacity: 0,
-    force3D: false
-  }, {
-    scale: 1,
-    y: "-100%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 1)",
-    force3D: false
-  }),
-]);
-
-var bTM = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerHook: 0,
-  })
-
-  .setTween(bTM)
-  .addTo(GeneralSM)
-  
-var bOutTM = new TimelineMax();
-
-bOutTM.add([
-  TweenMax.fromTo(".ink-b-js", 1, {
-    scale: 1,
-    y: "-100%",
-    position: "fixed",
-    opacity: 1,
-    force3D: false
-  }, {
-    scale: 130,
-    y: "-100%",
-    position: "fixed",
-    opacity: 1,
-    force3D: false,
-    opacity: 1,
-    ease: "expoScale(0, 130)",
-  }),
-]);
-
-var bOutTM = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-b',
-    triggerHook: 0,
-  })
-
-  .setTween(bOutTM)
-  .addTo(GeneralSM)
-
-// FIN BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-
-
-
-
-// CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-
-
-var Before = new TimelineMax();
-var Enter = new TimelineMax();
-var Here = new TimelineMax();
-var After = new TimelineMax();
-
-Before.add([
-  TweenMax.fromTo(".ink-c-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-200%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0,
-    opacity: 1,
-    y: "-200%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0)",
-    force3D: false
-  }),
-]);
-
-var Before = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-a',
-    triggerHook: 1,
-  })
-
-  .setTween(Before)
-  .addTo(GeneralSM)
-
-Enter.add([
-  TweenMax.fromTo(".ink-c-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-200%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0.02,
-    opacity: 1,
-    y: "-200%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0.02)",
-    force3D: false
-  }),
-]);
-
-var Enter = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-a',
-    triggerHook: -1,
-  })
-
-  .setTween(Enter)
-  .addTo(GeneralSM)
-
-Here.add([
-  TweenMax.fromTo(".ink-c-js", 1, {
-    scale: 0.02,
-    y: "-200%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 1,
-    y: "-200%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0.02, 1)",
-    force3D: false
-  }),
-]);
-
-var Here = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-b',
-    triggerHook: 0,
-  })
-
-  .setTween(Here)
-  .addTo(GeneralSM)
-
-After.add([
-  TweenMax.fromTo(".ink-c-js", 1, {
-    scale: 1,
-    y: "-200%",
-    position: "fixed",
-    opacity: 1,
-    force3D: false
-  }, {
-    scale: 130,
-    y: "-200%",
-    opacity: 1,
-    force3D: false,
-    opacity: 1,
-    ease: "expoScale(1, 130)",
-  }),
-]);
-
-var After = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-c',
-    triggerHook: 0,
-  })
-
-  .setTween(After)
-  .addTo(GeneralSM)
-
-
-// FIN CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-
-
-
-
-
-
-
-// DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
-
-
-var Before = new TimelineMax();
-var Enter = new TimelineMax();
-var Here = new TimelineMax();
-var After = new TimelineMax();
-
-Before.add([
-  TweenMax.fromTo(".ink-d-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-300%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0,
-    opacity: 1,
-    y: "-300%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0)",
-    force3D: false
-  }),
-]);
-var Before = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-b',
-    triggerHook: 1,
-  })
-
-  .setTween(Before)
-  .addTo(GeneralSM)
-
-Enter.add([
-  TweenMax.fromTo(".ink-d-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-300%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0.02,
-    opacity: 1,
-    y: "-300%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0.02)",
-    force3D: false
-  }),
-]);
-
-var Enter = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-b',
-    triggerHook: -1,
-  })
-
-  .setTween(Enter)
-  .addTo(GeneralSM)
-
-Here.add([
-  TweenMax.fromTo(".ink-d-js", 1, {
-    scale: 0.02,
-    y: "-300%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 1,
-    y: "-300%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0.02, 1)",
-    force3D: false
-  }),
-]);
-
-var Here = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-c',
-    triggerHook: 0,
-  })
-
-  .setTween(Here)
-  .addTo(GeneralSM)
-
-After.add([
-  TweenMax.fromTo(".ink-d-js", 1, {
-    scale: 1,
-    y: "-300%",
-    position: "fixed",
-    opacity: 1,
-    force3D: false
-  }, {
-    scale: 130,
-    y: "-300%",
-    opacity: 1,
-    force3D: false,
-    opacity: 1,
-    ease: "expoScale(1, 130)",
-  }),
-]);
-
-var After = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-d',
-    triggerHook: 0,
-  })
-
-  .setTween(After)
-  .addTo(GeneralSM)
-
-
-// FIN DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
-
-
-
-
-
-
-// EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-
-
-var Before = new TimelineMax();
-var Enter = new TimelineMax();
-var Here = new TimelineMax();
-var After = new TimelineMax();
-
-Before.add([
-  TweenMax.fromTo(".ink-e-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-400%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0,
-    opacity: 1,
-    y: "-400%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0)",
-    force3D: false
-  }),
-]);
-var Before = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-a',
-    triggerHook: 1,
-  })
-
-  .setTween(Before)
-  .addTo(GeneralSM)
-
-Enter.add([
-  TweenMax.fromTo(".ink-e-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-400%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0.02,
-    opacity: 1,
-    y: "-400%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0.02)",
-    force3D: false
-  }),
-]);
-
-var Enter = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-c',
-    triggerHook: -1,
-  })
-
-  .setTween(Enter)
-  .addTo(GeneralSM)
-
-Here.add([
-  TweenMax.fromTo(".ink-e-js", 1, {
-    scale: 0.02,
-    y: "-400%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 1,
-    y: "-400%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0.02, 1)",
-    force3D: false
-  }),
-]);
-
-var Here = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-d',
-    triggerHook: 0,
-  })
-
-  .setTween(Here)
-  .addTo(GeneralSM)
-
-After.add([
-  TweenMax.fromTo(".ink-e-js", 1, {
-    scale: 1,
-    y: "-400%",
-    position: "fixed",
-    opacity: 1,
-    force3D: false
-  }, {
-    scale: 130,
-    y: "-400%",
-    opacity: 1,
-    force3D: false,
-    opacity: 1,
-    ease: "expoScale(1, 130)",
-  }),
-]);
-
-var After = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-e',
-    triggerHook: 0,
-  })
-
-  .setTween(After)
-  .addTo(GeneralSM)
-
-
-// FIN EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-
-
-
-
-// EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-
-
-var Before = new TimelineMax();
-var Enter = new TimelineMax();
-var Here = new TimelineMax();
-var After = new TimelineMax();
-
-Before.add([
-  TweenMax.fromTo(".ink-f-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-500%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0,
-    opacity: 1,
-    y: "-500%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0)",
-    force3D: false
-  }),
-]);
-var Before = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-a',
-    triggerHook: 1,
-  })
-
-  .setTween(Before)
-  .addTo(GeneralSM)
-
-Enter.add([
-  TweenMax.fromTo(".ink-f-js", 1, {
-    scale: 0,
-    opacity: -0.5,
-    y: "-500%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 0.02,
-    opacity: 1,
-    y: "-500%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0, 0.02)",
-    force3D: false
-  }),
-]);
-
-var Enter = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-d',
-    triggerHook: -1,
-  })
-
-  .setTween(Enter)
-  .addTo(GeneralSM)
-
-Here.add([
-  TweenMax.fromTo(".ink-f-js", 1, {
-    scale: 0.02,
-    y: "-500%",
-    position: "fixed",
-    force3D: false
-  }, {
-    scale: 1,
-    y: "-500%",
-    position: "fixed",
-    opacity: 1,
-    ease: "expoScale(0.02, 1)",
-    force3D: false
-  }),
-]);
-
-var Here = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-e',
-    triggerHook: 0,
-  })
-
-  .setTween(Here)
-  .addTo(GeneralSM)
-
-After.add([
-  TweenMax.fromTo(".ink-f-js", 1, {
-    scale: 1,
-    y: "-500%",
-    position: "fixed",
-    opacity: 1,
-    force3D: false
-  }, {
-    scale: 130,
-    y: "-500%",
-    opacity: 1,
-    force3D: false,
-    opacity: 1,
-    ease: "expoScale(1, 130)",
-  }),
-]);
-
-var After = new ScrollMagic.Scene({
-    duration: '100%',
-    triggerElement: '.ink-f',
-    triggerHook: 0,
-  })
-
-  .setTween(After)
-  .addTo(GeneralSM)
-
-
-// FIN EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-}
+gsap.registerPlugin(ScrollTrigger)
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // wait until images, links, fonts, stylesheets, and js is loaded
+    window.addEventListener("load", function (e) {
+
+        gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".ink-a-js",
+                    pin: true,
+                    scrub: 1,
+                    start: "top",
+                    end: "bottom",
+                    markers: true
+                }
+            })
+            .fromTo(".ink-a-js", {
+                scale: 1,
+                y: "0%",
+                position: "fixed",
+                force3D: false
+            }, {
+                scale: 130,
+                y: "0%",
+                position: "fixed",
+                ease: "expoScale(1, 130)",
+                force3D: false
+            }, 0);
+
+        gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".ink-b-js",
+                    pin: true,
+                    scrub: 1,
+                    start: "top",
+                    end: "bottom",
+                    markers: true
+                }
+            })
+            .fromTo(".ink-b-js", {
+                scale: 1,
+                y: "0%",
+                position: "fixed",
+                force3D: false
+            }, {
+                scale: 130,
+                y: "0%",
+                position: "fixed",
+                ease: "expoScale(1, 130)",
+                force3D: false
+            }, 0);
+
+        gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".ink-c-js",
+                    pin: true,
+                    scrub: 1,
+                    start: "top",
+                    end: "bottom",
+                    markers: true
+                }
+            })
+            .fromTo(".ink-c-js", {
+                scale: 1,
+                y: "0%",
+                position: "fixed",
+                force3D: false
+            }, {
+                scale: 130,
+                y: "0%",
+                position: "fixed",
+                ease: "expoScale(1, 130)",
+                force3D: false
+            }, 0);
+
+        gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".ink-d-js",
+                    pin: true,
+                    scrub: 1,
+                    start: "top",
+                    end: "bottom",
+                    markers: true
+                }
+            })
+            .fromTo(".ink-d-js", {
+                scale: 1,
+                y: "0%",
+                position: "fixed",
+                force3D: false
+            }, {
+                scale: 130,
+                y: "0%",
+                position: "fixed",
+                ease: "expoScale(1, 130)",
+                force3D: false
+            }, 0);
+
+        gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".ink-e-js",
+                    pin: true,
+                    scrub: 1,
+                    start: "top",
+                    end: "bottom",
+                    markers: true
+                }
+            })
+            .fromTo(".ink-e-js", {
+                scale: 1,
+                y: "0%",
+                position: "fixed",
+                force3D: false
+            }, {
+                scale: 130,
+                y: "0%",
+                position: "fixed",
+                ease: "expoScale(1, 130)",
+                force3D: false
+            }, 0);
+
+    });
+
+})
