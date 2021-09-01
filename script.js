965a47333b36899fe20c2ad8ca1349d4dde95c1a
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2021-09-02 00:29:26 +0200

    congratulation

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
-gsap.registerPlugin(ScrollTrigger, EasePack)
+gsap.registerPlugin(ScrollTrigger, EasePack);
 
-// wait until DOM is ready
-document.addEventListener("DOMContentLoaded", function (event) {
-    // wait until images, links, fonts, stylesheets, and js is loaded
-        window.addEventListener("load", function (e) {
+gsap.config({force3D: false});
 
-            gsap.utils.toArray(".section").forEach((item, index, arr) => {
-                let next = ((arr.length - 1 === index)? "END" : arr[index+1]);
+let tl = gsap.timeline({
+  scrollTrigger: {
+    start: 0,
+    end: "max",
+    scrub: 2
+  }
+});
 
-                const A = gsap.timeline({
-                    scrollTrigger: {
-                        trigger: item,
-                        start: "top-=1000%",
-                        end: "bottom",
-                        pin: true,
-                        scrub: 2,                        
-                        //snap: arr.length / arr.length,
-                         markers: {startColor: "green", endColor: "green", fontSize: "12px"},
-                    }
-                })    
-
-
-                A.set( item,{
-                    scale: 0,
-                    y: 0,
-                    position: "fixed",
-                    force3D: false,                                    
-                })
-
-                A.fromTo( item,{
-                    scale: 0,
-                    y: 0,
-                    position: "fixed",
-                    force3D: false,                                    
-                }, {
-                    scale: 250,
-                    y: 0,
-                    position: "fixed",
-                    ease: "expoScale(1, 15400000000000)",
-                    force3D: false,                                    
-                }, 0)
-            });
-    })
-})
+gsap.utils.toArray("section").forEach((section, i) => {
+  if (i) {
+    tl.fromTo(section, {scale: 0}, {
+      scale: 1,
+      ease: "none" // or "expoScale(0.01, 1)" but that didn't seem quite as nice because it takes a while to become visible.
+    }, "<") // completely overlap with the previous tween
+  }
+  tl.to(section, {
+    scale: 200,
+    ease: "expoScale(1, 200)"
+  });
+});
